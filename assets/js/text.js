f6087250fca89f35870839e9324c731b09fce787
Akrifstg <akrifisofficial@gmail.com>
2025-07-31 01:53:22 +0600

    Update text.js

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".typed").typed({
-        strings: ["Quarte", "Termuxtu3tor", "Hello world!", "Telegram Developer", "nedoкодер", "hmmm...", "bruh", "Kto eto?", "h@Cк3D!"],
+        strings: ["Akrifis", "Termuxtu3tor", "Hello world!", "Telegram Developer", "nedoкодер", "hmmm...", "bruh", "Kto eto?", "h@Cк3D!"],
         typeSpeed: 30,
         backSpeed: 0,
         cursorChar: ['❚'],
